d68d75923b99c624f370fcc071af23bd989ef9a3
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-06-17 15:16:22 -0500

    upper left half done

--- Assignment/vispums.py
+++ Assignment/vispums.py
@@ -19,19 +19,24 @@ pums_dataframe = pd.read_csv('ss13hil.csv')
 fig, axs = plt.subplots(2, 2)
 
 # Set size of figure for display
-fig.set_size_inches(20,16)
+fig.set_size_inches(20,20)
 
-# TODO Upper Left Subplot - Pie Chart contaning num of household records for the different values of HHL Column
-# > No wedge labels, must have legend in upper left corner, correctly rotated
+# Upper Left Subplot - Pie Chart contaning num of household records for the different values of HHL Column
+# TODO fix circle so not oblong
 pie_key=['English','Spanish','Other Indo-European','Asian and Pacific Island languages','Other']
 axs[0,0].set_title('Household Languages')
 axs[0,0].pie(pums_dataframe.HHL.value_counts().dropna(),startangle=240)
 axs[0,0].legend(pie_key,loc="upper left")
 axs[0,0].set(ylabel='HHL')
 
-# TODO Upper Right Subplot - Histogram of HINCP Column with KDE plot superimposed
-# > Use log scale on the x-axis with log spaced bins [np.logspace]
-
+# Upper Right Subplot - Histogram of HINCP Column with KDE plot superimposed
+# TODO fix y axis lables, add --k lines, add data after 10^3
+x = pums_dataframe.HINCP.dropna().value_counts()
+axs[0,1].set_title('Distribution of Household Income')
+axs[0,1].hist(x,bins=np.logspace(1,7),facecolor='green',alpha=.50)
+axs[0,1].set(xlabel='Household Income($)- Log Scaled',ylabel='Density')
+axs[0,1].set_xscale("log")
+axs[0,1].grid(linestyle='dotted')
 
 # TODO Lower Left Subplot - Bar Chart of number of households in thousands for each VEH value[drop NaN]
 # > Use WGTP value to count how many households are in each row then divide sum by 1000 to get households in thousands
