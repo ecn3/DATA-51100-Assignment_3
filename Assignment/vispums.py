b9cefd84cc1ba229b21202a2bafdab976ad79813
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-06-17 15:18:36 -0500

    correct lables Q3

--- Assignment/vispums.py
+++ Assignment/vispums.py
@@ -29,7 +29,7 @@ axs[0,0].pie(pums_dataframe.HHL.value_counts().dropna(),startangle=240)
 axs[0,0].legend(pie_key,loc="upper left")
 axs[0,0].set(ylabel='HHL')
 
-# Upper Right Subplot - Histogram of HINCP Column with KDE plot superimposed
+#  Upper Right Subplot - Histogram of HINCP Column with KDE plot superimposed
 # TODO fix y axis lables, add --k lines, add data after 10^3
 x = pums_dataframe.HINCP.dropna().value_counts()
 axs[0,1].set_title('Distribution of Household Income')
@@ -38,8 +38,11 @@ axs[0,1].set(xlabel='Household Income($)- Log Scaled',ylabel='Density')
 axs[0,1].set_xscale("log")
 axs[0,1].grid(linestyle='dotted')
 
-# TODO Lower Left Subplot - Bar Chart of number of households in thousands for each VEH value[drop NaN]
-# > Use WGTP value to count how many households are in each row then divide sum by 1000 to get households in thousands
+# Lower Left Subplot - Bar Chart of number of households in thousands for each VEH value[drop NaN]
+# TODO fix x-axis and y-axis lables, display correct information
+axs[1,0].set_title('Vehicles Available in Households')
+axs[1,0].bar(pums_dataframe.groupby('VEH').WGTP.mean()/1000,pums_dataframe.VEH.value_counts().dropna(),facecolor='red')
+axs[1,0].set(xlabel='# of Vehicles',ylabel='Thousands of Households')
 
 # TODO Lower Right Subplot - Scatter plot of TAXP against VALP
 # > convert TAXP into numeric values, use lower bounds interval, Use WGTP as the size of each marker, 'o' as marker type, and MRGP as the color value, add colorbar
