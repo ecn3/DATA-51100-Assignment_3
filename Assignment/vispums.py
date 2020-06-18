cee0cdabbee5a17db46237ca575b0fda1209eb6d
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-06-18 00:19:43 -0500

    added title and hspace adjustment

--- Assignment/vispums.py
+++ Assignment/vispums.py
@@ -18,6 +18,9 @@ pums_dataframe = pd.read_csv('ss13hil.csv')
 # Create a figure with 2x2 subplots
 fig, axs = plt.subplots(2, 2)
 
+# Adjust hspace for proper veiwing
+fig.subplots_adjust(hspace=0.32)
+
 # Upper Left Subplot - Pie Chart contaning num of household records for the different values of HHL Column
 # TODO fix circle so not oblong
 pie_key=['English','Spanish','Other Indo-European','Asian and Pacific Island languages','Other']
@@ -27,7 +30,7 @@ axs[0,0].legend(pie_key,loc="upper left")
 axs[0,0].set(ylabel='HHL')
 
 #  Upper Right Subplot - Histogram of HINCP Column with KDE plot superimposed
-# Me TODO fix y axis lables, add --k lines, add data after 10^3, fix lower label
+# TODO KDE plot superimposed
 # Set the title
 axs[0,1].set_title('Distribution of Household Income', fontsize=8)
 # Set the x label
@@ -55,9 +58,11 @@ pums_bar = pums_dataframe.groupby('VEH')['WGTP'].sum()/1000
 # Display Data
 axs[1,0].bar(pums_bar.index,pums_bar.values,facecolor='red')
 
+
 # Lower Right Subplot - Scatter plot of TAXP against VALP
 # Me TODO fix x and y axis, fix saturation, add color bar, add right label, fix upper label, fix data
-
+# Set the title
+axs[1,1].set_title('Property Taxes vs Property Values', fontsize=8)
 
 # Display figure
 plt.show()
