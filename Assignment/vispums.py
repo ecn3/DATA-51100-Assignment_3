ef103d9d6f694567ba649b32f4f225906bfb8f1e
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-06-17 22:56:28 -0500

    working q3

--- Assignment/vispums.py
+++ Assignment/vispums.py
@@ -27,25 +27,29 @@ axs[0,0].legend(pie_key,loc="upper left")
 axs[0,0].set(ylabel='HHL')
 
 #  Upper Right Subplot - Histogram of HINCP Column with KDE plot superimposed
-# TODO fix y axis lables, add --k lines, add data after 10^3, fix lower label
-x = pums_dataframe.HINCP.dropna().value_counts()
-axs[0,1].set_title('Distribution of Household Income')
-axs[0,1].hist(x,bins=np.logspace(1,7),facecolor='green',alpha=.50)
-axs[0,1].set(xlabel='Household Income($)- Log Scaled',ylabel='Density')
-axs[0,1].set_xscale("log")
-axs[0,1].grid(linestyle='dotted')
+# Me TODO fix y axis lables, add --k lines, add data after 10^3, fix lower label
+# Set the title
+axs[0,1].set_title('Distribution of Household Income', fontsize=8)
+# Set the x label
+axs[0,1].set_xlabel('Household Income($)- Log Scaled', fontsize=8)
+# Set the y label
+axs[0,1].set_ylabel('Density', fontsize=8)
 
 # Lower Left Subplot - Bar Chart of number of households in thousands for each VEH value[drop NaN]
-# TODO fix x-axis and y-axis lables, display correct information
-axs[1,0].set_title('Vehicles Available in Households')
-axs[1,0].bar(pums_dataframe.groupby('VEH').WGTP.mean()/1000,pums_dataframe.VEH.value_counts().dropna(),facecolor='red')
-axs[1,0].set(xlabel='# of Vehicles',ylabel='Thousands of Households')
+# Set the title
+axs[1,0].set_title('Vehicles Available in Households', fontsize=8)
+# Set the x label
+axs[1,0].set_xlabel('# of Vehicles', fontsize=8)
+# Set the y label
+axs[1,0].set_ylabel('Thousands of Households', fontsize=8)
+# Get data
+pums_bar = pums_dataframe.groupby('VEH')['WGTP'].sum()/1000
+# Display Data
+axs[1,0].bar(pums_bar.index,pums_bar.values,facecolor='red')
 
 # Lower Right Subplot - Scatter plot of TAXP against VALP
-# TODO fix x and y axis, fix saturation, add color bar, add right label, fix upper label, fix data
-axs[1,1].set_title('Property Taxes vs Property Values')
-axs[1,1].scatter(pums_dataframe.VALP, pums_dataframe.TAXP)
-axs[1,1].set(xlabel='Property Values($)',ylabel='Taxes')
+# Me TODO fix x and y axis, fix saturation, add color bar, add right label, fix upper label, fix data
+
 
 # Display figure
 plt.show()
