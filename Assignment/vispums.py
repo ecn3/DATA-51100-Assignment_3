75f27d3c476f8ce95ad15649d55612f44148f51a
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-06-19 20:07:56 -0500

    colors mostly right

--- Assignment/vispums.py
+++ Assignment/vispums.py
@@ -76,9 +76,9 @@ taxp_conversions = pd.Series([None, 1,50,100,150,200,250,300,350,400,450,500,550
 for y in range(1,69):
     pums_scatter = pums_scatter.replace(to_replace= y, value= taxp_conversions[y-1])     
 # Create colormap
-cmap = mpl.colors.LinearSegmentedColormap.from_list("", ["blue","pink"])
+cmap = mpl.colors.LinearSegmentedColormap.from_list("", ["lightblue","white","pink"])
 # Graph Data
-scatter_data = axs[1,1].scatter(pums_scatter.VALP,pums_scatter.TAXP,marker='o',s=pums_scatter.WGTP/400, c=pums_scatter.MRGP,cmap=cmap)
+scatter_data = axs[1,1].scatter(pums_scatter.VALP,pums_scatter.TAXP,marker='o',s=pums_scatter.WGTP/200, c=pums_scatter.MRGP,cmap=cmap)
 axs[1,1].set_xlim(0,1200000)
 # Add color bar and label
 cb = plt.colorbar(scatter_data)
