c33896e8dffa0afca48be369e5402b0cfa58b56a
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-06-19 23:22:55 -0500

    Q4

--- Assignment/vispums.py
+++ Assignment/vispums.py
@@ -22,13 +22,12 @@ fig, axs = plt.subplots(2, 2)
 fig.subplots_adjust(hspace=0.32)
 
 # Upper Left Subplot - Pie Chart contaning num of household records for the different values of HHL Column
-# TODO fix circle so not oblong
 pie_key=['English','Spanish','Other Indo-European','Asian and Pacific Island languages','Other']
 axs[0,0].set_title('Household Languages', fontsize=8)
-axs[0,0].pie(pums_dataframe.HHL.value_counts().dropna(),startangle=240)
-axs[0,0].legend(pie_key,loc="upper left")
+axs[0,0].pie(pums_dataframe.HHL.value_counts().dropna(),startangle=242)
+axs[0,0].legend(pie_key,loc=2, fontsize=8)
 axs[0,0].set_ylabel('HHL', fontsize=8)
-
+axs[0,0].axis('equal')
 
 #  Upper Right Subplot - Histogram of HINCP Column with KDE plot superimposed
 # Set the title
@@ -70,10 +69,17 @@ axs[1,1].set_xlabel('Property Values($)', fontsize=8)
 axs[1,1].set_ylabel('Taxes($)', fontsize=8)
 # Get data
 pums_scatter = pums_dataframe[['TAXP','VALP','WGTP','MRGP']].dropna()
-# TODO Convert TAXP using interval
-
+# Convert TAXP using interval
+taxp_conversions = pd.Series([None, 1,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,
+1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,3100,3200,3300,
+3400,3500,3600,3700,3800,3900,4000,4100,4200,4300,4400,4500,4600,4700,4800,4900,500,5500,6000,7000,8000,900,10000 ])
+for y in range(1,69):
+    pums_scatter = pums_scatter.replace(to_replace= y, value= taxp_conversions[y-1])     
+# Create colormap
+cmap = mpl.colors.LinearSegmentedColormap.from_list("", ["lightblue","white","pink"])
 # Graph Data
-scatter_data = axs[1,1].scatter(pums_scatter.VALP,pums_scatter.TAXP,marker='o',s=pums_scatter.WGTP, c=pums_scatter.MRGP)
+scatter_data = axs[1,1].scatter(pums_scatter.VALP,pums_scatter.TAXP,marker='o',s=pums_scatter.WGTP/200, c=pums_scatter.MRGP,cmap=cmap)
+axs[1,1].set_xlim(0,1200000)
 # Add color bar and label
 cb = plt.colorbar(scatter_data)
 # Set color bar label
